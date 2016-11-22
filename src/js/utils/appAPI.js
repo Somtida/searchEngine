57a4be0c88ac476414f60f86bc1732973b0b76e0
S.SKNS <somtida.th@gmail.com>
2016-11-22 04:17:20 +0000

    Update appAPI.js

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,7 +1,19 @@
 var AppActions = require('../actions/AppActions');
 
 module.exports = {
-   searchText(search) {
+  searchText(search) {
     console.log('API searching for '+search.text);
+    var url = `https://api.duckduckgo.com/?q=${search.text}&format=json&pretty=1`;
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      cache: false,
+      success: function(data){
+        console.log(data.RelatedTopics)
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+      }.bind(this),
+    })
   }
 }
