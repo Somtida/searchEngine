0eaaf4a913326ac1a0d4fca290e3f3a468c6275f
S.SKNS <somtida.th@gmail.com>
2016-11-24 08:14:47 +0000

    Update AppStore.js

--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -35,7 +35,7 @@ AppDispatcher.register(function(payload) {
       break;
 
     case AppConstants.RECEIVE_RESULTS:
-      console.log('hello');
+      console.log(action.results);
       AppStore.emit(CHANGE_EVENT);
       break;
 
