f46cce43c91759f6fade26572686a18b7ad30a89
S.SKNS <somtida.th@gmail.com>
2016-11-30 16:11:06 -0800

    Update SearchResults.js

--- src/js/components/SearchResults.js
+++ src/js/components/SearchResults.js
@@ -4,11 +4,16 @@ var AppStore = require('../stores/AppStore');
 
 var SearchResults = React.createClass({
   render(){
+	if(this.porps.searchText != ‘’) {
+		var results = <h2 className=“page-header”>Results</h2>
+	} else {
+		var results = ‘’;
+	}
     return(
       <div className="search-form">
         
         <div>
-	  <h2 className=“page-header”>Results</h2>
+	  <h2 className=“page-header”>{results}</h2>
         </div>
 
       </div>
